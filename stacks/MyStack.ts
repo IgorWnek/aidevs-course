4796f7ac19221b478335bf8601acb35c43afc417
Igor Wnek <igowne@gmail.com>
2023-06-04 00:00:16 +0200

    feat: add slution to moderation task
    TODO refactor that code

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -3,7 +3,7 @@ import { StackContext, Api } from "sst/constructs";
 export function API({ stack }: StackContext) {
   const api = new Api(stack, "api", {
     routes: {
-      "GET /": "packages/functions/src/lambda.handler",
+      "GET /moderation": "packages/functions/src/moderation.handler",
     },
   });
   stack.addOutputs({
