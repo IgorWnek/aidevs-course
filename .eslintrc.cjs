bcfc25110c3371f545748f84e549d042eaccf1fd
Igor Wnek <igowne@gmail.com>
2023-06-04 14:54:09 +0200

    config: adjust configs of the Prettier and ESLint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,12 +4,17 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:@typescript-eslint/strict',
+    'prettier',
   ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   parserOptions: {
     project: ['./tsconfig.json', './packages/*/tsconfig.json'],
     tsconfigRootDir: __dirname,
+  },
+  rules: {
+    "@typescript-eslint/consistent-type-definitions": "off",
   },
   root: true,
 };
