9161bd20b60c4c09a511ba9037c630a62dcff7f7
Igor Wnek <igowne@gmail.com>
2023-06-05 22:39:35 +0200

    feat: move moderation task logic to the controller

--- packages/core/src/infrastructure/controller/solveModerationTask/SolveModerationTaskController.ts
+++ packages/core/src/infrastructure/controller/solveModerationTask/SolveModerationTaskController.ts
@@ -1,7 +1,12 @@
 import { AiDevsConfig } from '../../../../config/aiDevs/AiDevsConfig';
+import fetch, { RequestInit } from 'node-fetch';
+import { Configuration, OpenAIApi } from 'openai';
+import { ChatCompletionRequestMessage } from 'openai/api';
+import { OpenAiConfig } from '../../../../config/openAi/OpenAiConfig';
 
 export interface SolveModerationTaskDependencies {
   aiDevsConfig: AiDevsConfig;
+  openAiConfig: OpenAiConfig;
 }
 
 export interface SolveModerationTaskResponse {
@@ -11,17 +16,118 @@ export interface SolveModerationTaskResponse {
 }
 
 export class SolveModerationTaskController {
-  private aiDevsConfig: AiDevsConfig;
-
-  public constructor(dependencies: SolveModerationTaskDependencies) {
-    const { aiDevsConfig } = dependencies;
-
-    this.aiDevsConfig = aiDevsConfig;
-  }
+  public constructor(private dependencies: SolveModerationTaskDependencies) {}
 
   public async execute(): Promise<SolveModerationTaskResponse> {
-    return Promise.resolve({
-      content: 'Answer incorrect. ⛔ Time to bug hunting. 🪲',
+    const { aiDevsConfig, openAiConfig } = this.dependencies;
+
+    const aiDevsTasksUrl = 'https://zadania.aidevs.pl';
+    const taskName = 'moderation';
+    const aiDevsTaskTokenEndpointUrl = `${aiDevsTasksUrl}/token/${taskName}`;
+
+    const options: RequestInit = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        apikey: aiDevsConfig.apiKey,
+      }),
+    };
+
+    type TaskToken = {
+      code: number;
+      msg: string;
+      token: string;
+    };
+
+    const taskTokenResponse = await fetch(aiDevsTaskTokenEndpointUrl, options);
+    const taskToken = (await taskTokenResponse.json()) as TaskToken;
+
+    const aiDevsTaskEndpointUrl = `${aiDevsTasksUrl}/task/${taskToken.token}`;
+    const taskEndpointOptions: RequestInit = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    type ModerationTask = {
+      code: number;
+      msg: string;
+      input: [string, string, string, string];
+    };
+
+    const taskResponse = await fetch(
+      aiDevsTaskEndpointUrl,
+      taskEndpointOptions
+    );
+    const moderationTask = (await taskResponse.json()) as ModerationTask;
+
+    // Business Logic
+    const openaiConfiguration = new Configuration({
+      organization: openAiConfig.organization,
+      apiKey: openAiConfig.apiKey,
     });
+    const openai = new OpenAIApi(openaiConfiguration);
+    const prompt = `
+Zachowuj się jak doświadczony moderator treści, który rygorystycznie rozpoznaje czy dana treść powinna być moderowana czy nie. Twoje odpowiedzi powinny zawierać tylko i wyłącznie tablicę sformatowaną zgodnie z instrukcjami w Kontekście.
+
+### Kontekst
+
+Otrzymasz tablicę z pewnymi zdaniami lub wyrażeniami.
+Twoim zadaniem jest sklasyfikowanie każdego zadania czy powinno być moderowane czy nie.
+Klasyfikacji dokonujesz przez podanie "1" jeżeli treść musi być zmoderowana lub "0" jeżeli nie.
+
+Przykładowe zdania:
+['lubię placki z dżemem', 'piękne kotki piją mleczko', 'Trzeba zbić to dziecko!', 'ale piękne niebo']
+
+Przykładowa odpowiedź:
+[0,0,1,0]
+
+### Zadanie
+${JSON.stringify(moderationTask.input)}
+  `;
+
+    const message: ChatCompletionRequestMessage = {
+      role: 'user',
+      content: prompt,
+    };
+    const completionResponse = await openai.createChatCompletion({
+      messages: [message],
+      model: 'gpt-4',
+    });
+
+    const chatResponseContent =
+      completionResponse.data.choices[0].message?.content;
+    let moderationPhrases = [] as number[];
+
+    if (chatResponseContent !== undefined) {
+      moderationPhrases = JSON.parse(chatResponseContent) as number[];
+    }
+
+    // Verify answer
+    const answerEndpointUrl = `${aiDevsTasksUrl}/answer/${taskToken.token}`;
+    const answerRequestOptions: RequestInit = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        answer: moderationPhrases,
+      }),
+    };
+
+    const answerResponse = await fetch(answerEndpointUrl, answerRequestOptions);
+
+    if (answerResponse.ok) {
+      return {
+        content: `Answer correct! 🎉🎉🎉`,
+      };
+    }
+
+    return {
+      content: `Answer incorrect. ⛔ Time to bug hunting. 🪲`,
+    };
   }
 }
--- packages/functions/src/moderation.ts
+++ packages/functions/src/moderation.ts
@@ -1,116 +1,12 @@
-import { ApiHandler } from "sst/node/api";
-import { Time } from "@aidevs-course/core/time";
-import { APIGatewayProxyEventV2 } from "aws-lambda";
-import "dotenv/config";
-import fetch, { RequestInit } from "node-fetch";
-import { Configuration, OpenAIApi } from "openai";
-import { ChatCompletionRequestMessage } from "openai/api";
+import { ApiHandler } from 'sst/node/api';
+import { APIGatewayProxyEventV2 } from 'aws-lambda';
+import 'dotenv/config';
+import { solveModerationTaskController } from '@aidevs-course/core/infrastructure/controller/solveModerationTask';
 
 export const handler = ApiHandler(async (event: APIGatewayProxyEventV2) => {
-  const aiDevsApiKey = process.env.AI_DEVS_API_KEY;
-  const aiDevsTasksUrl = "https://zadania.aidevs.pl";
-  const taskName = "moderation";
-  const aiDevsTaskTokenEndpointUrl = `${aiDevsTasksUrl}/token/${taskName}`;
-
-  const options: RequestInit = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      apikey: aiDevsApiKey,
-    }),
-  };
-
-  type TaskToken = {
-    code: number;
-    msg: string;
-    token: string;
-  };
-
-  const taskTokenResponse = await fetch(aiDevsTaskTokenEndpointUrl, options);
-  const taskToken = (await taskTokenResponse.json()) as TaskToken;
-
-  const aiDevsTaskEndpointUrl = `${aiDevsTasksUrl}/task/${taskToken.token}`;
-  const taskEndpointOptions: RequestInit = {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
-
-  type ModerationTask = {
-    code: number;
-    msg: string;
-    input: [string, string, string, string];
-  };
-
-  const taskResponse = await fetch(aiDevsTaskEndpointUrl, taskEndpointOptions);
-  const moderationTask = (await taskResponse.json()) as ModerationTask;
-
-  // Business Logic
-  const openaiConfiguration = new Configuration({
-    organization: process.env.OPENAI_ORGANIZATION_ID,
-    apiKey: process.env.OPENAI_API_KEY,
-  });
-  const openai = new OpenAIApi(openaiConfiguration);
-  const prompt = `
-Zachowuj się jak doświadczony moderator treści, który rygorystycznie rozpoznaje czy dana treść powinna być moderowana czy nie. Twoje odpowiedzi powinny zawierać tylko i wyłącznie tablicę sformatowaną zgodnie z instrukcjami w Kontekście.
-
-### Kontekst
-
-Otrzymasz tablicę z pewnymi zdaniami lub wyrażeniami.
-Twoim zadaniem jest sklasyfikowanie każdego zadania czy powinno być moderowane czy nie.
-Klasyfikacji dokonujesz przez podanie "1" jeżeli treść musi być zmoderowana lub "0" jeżeli nie.
-
-Przykładowe zdania:
-['lubię placki z dżemem', 'piękne kotki piją mleczko', 'Trzeba zbić to dziecko!', 'ale piękne niebo']
-
-Przykładowa odpowiedź:
-[0,0,1,0]
-
-### Zadanie
-${JSON.stringify(moderationTask.input)}
-  `;
-
-  const message: ChatCompletionRequestMessage = {
-    role: "user",
-    content: prompt,
-  };
-  const completionResponse = await openai.createChatCompletion({
-    messages: [message],
-    model: "gpt-4",
-  });
-
-  const chatResponseContent =
-    completionResponse.data.choices[0].message?.content;
-  let moderationPhrases = [] as number[];
-
-  if (chatResponseContent) {
-    moderationPhrases = JSON.parse(chatResponseContent) as number[];
-  }
-
-  // Verify answer
-  const answerEndpointUrl = `${aiDevsTasksUrl}/answer/${taskToken.token}`;
-  const answerRequestOptions: RequestInit = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      answer: moderationPhrases,
-    }),
-  };
-
-  const answerResponse = await fetch(answerEndpointUrl, answerRequestOptions);
-
-  if (answerResponse.ok) {
-    return {
-      body: `Answer correct! 🎉🎉🎉`,
-    };
-  }
+  const response = await solveModerationTaskController.execute();
 
   return {
-    body: `Answer incorrect. ⛔ Time to bug hunting. 🪲`,
+    body: response.content,
   };
 });
