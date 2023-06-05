96c16004bee129e57eea70ff5cc21da29410cfc1
Igor Wnek <igowne@gmail.com>
2023-06-05 22:41:35 +0200

    feat: inject openAiConfig to the moderation task controller

--- packages/core/src/infrastructure/controller/solveModerationTask/index.ts
+++ packages/core/src/infrastructure/controller/solveModerationTask/index.ts
@@ -1,8 +1,10 @@
 import { SolveModerationTaskController } from './SolveModerationTaskController';
 import { aiDevsConfig } from '../../../../config/aiDevs';
+import { openAiConfig } from '../../../../config/openAi';
 
 const solveModerationTaskController = new SolveModerationTaskController({
   aiDevsConfig,
+  openAiConfig,
 });
 
 export { solveModerationTaskController };
