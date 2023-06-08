04e769880a8f8cce9c3331366ba88f2465f477a8
Igor Wnek <igowne@gmail.com>
2023-06-08 22:15:53 +0200

    test: add TODO promising that tests will be made after two more AiDevs tasks

--- packages/core/src/application/useCase/solveModerationTask/SolveModerationTaskUseCase.test.ts
+++ packages/core/src/application/useCase/solveModerationTask/SolveModerationTaskUseCase.test.ts
@@ -5,6 +5,7 @@ import { TaskResult } from './SolveTaskUseCase';
 describe('Given Moderation Task Use Case', () => {
   describe('When task is solved correctly', () => {
     it('Then use-case returns truthy response', async () => {
+      // TODO: create proper tests after finishing two more tasks
       const taskResult = await solveModerationTaskUseCase.execute();
       const expectedTaskResult: TaskResult = {
         answeredCorrect: true,
